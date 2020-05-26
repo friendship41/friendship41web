5e25574ea7b1134f4e2041736b3cc99661084228
ht-chchoi <chchoi@hdtel.co.kr>
2020-05-26 14:43:58 +0900

    Initial commit

---
+++ src/main/java/com/friendship41/web/friendship41web/Friendship41webApplication.java
@@ -0,0 +1,15 @@
+package com.friendship41.web.friendship41web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Friendship41webApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Friendship41webApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/friendship41/web/friendship41web/Friendship41webApplicationTests.java
@@ -0,0 +1,15 @@
+package com.friendship41.web.friendship41web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Friendship41webApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
